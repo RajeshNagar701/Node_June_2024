fb281ac4b8c69eeee9c21169d8cf0b2e1bfef98a
GauravGoswami12967 <gaurav12967@gmail.com>
2024-09-13 17:10:15 +0530

    changes

---
+++ Node Js/10 Mongoose_Node/basic_mongoose_crud_api/index.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/node9th');
+
+const express=require('express');
+const app=express();
+app.use(express.json());
+
+// Schema means table coumn validation
+const userSchema= mongoose.Schema({
+    id:Number,
+    name:String,
+    email:String,
+    password:String
+});
+
+// create model/table appy validation on table coumn
+const userModel=mongoose.model("user",userSchema);
+
+
+// insert data by schema
+app.post("/insert", async (req, resp) => {
+    let data = new userModel(req.body)
+    const result=await data.save();
+    resp.send(result);
+})
+
+// get all data
+app.get("/get", async (req, resp) => {
+    let data = await userModel.find();
+    resp.send(data);
+})
+
+// get Single data by column
+
+app.get("/single", async (req, resp) => {
+    let data = await userModel.find({name:"raj nagar"});
+    resp.send(data);
+})
+
+//put http://localhost:5000/update/637f3ba97f546bbfeae336c3
+
+app.put("/update/:_id",async (req, resp) => {
+    console.log(req.params)
+    let data = await userModel.updateOne(
+        req.params,
+        {$set: req.body}
+    );
+    resp.send(data);
+})
+
+//delete  http://localhost:5000/delete/637f3ba97f546bbfeae336c3
+app.delete("/delete/:_id", async (req, resp) => {
+    console.log(req.params)
+    let data = await userModel.deleteOne(req.params);
+    resp.send(data);
+})
+
+app.listen(5000);
---
+++ Node Js/9 Mongo_Node/2)mongo_node_Api/index.js
@@ -0,0 +1,76 @@
+
+const dbConnect=require('./MongoConn');
+const express=require('express');
+const app=express();
+
+
+app.use(express.json());
+
+// get all data  http://localhost:5000/getdata
+app.get('/getdata', async (req, resp) => {
+    let conn = await dbConnect();
+    data = await conn.find().toArray();
+    resp.send(data);
+});
+
+//http://localhost:5000/getsingle2?id=5
+
+// get single or particular column data 
+app.get('/getsingle1', async (req, resp) => {
+    let conn = await dbConnect();
+    data = await conn.find({id:10}).toArray();
+    resp.send(data);
+});
+
+//http://localhost:5000/getsingle2?name=raj nagar
+// get single or particular column data
+app.get('/getsingle2', async (req, resp) => {
+    let conn = await dbConnect();
+    data = await conn.find(req.query).toArray();
+	console.log(req.query);
+    resp.send(data);
+});
+
+
+// data insert static   http://localhost:5000/postdata1
+app.get("/postdata1", async (req, resp) => {
+    let data = await dbConnect();
+    let result = await data.insertOne({id: 9,name:'9patel',email:'9patel@gmail.com',password:'71234'})
+    resp.send({result:"Static Data Inserted Success"});
+})
+
+
+// data insert dynamic http://localhost:5000/postdata2  add data in body
+
+app.post("/postdata2", async (req, resp) => {
+	let data = await dbConnect();
+    let result = await data.insertOne(req.body)
+	console.log(req.body);
+    resp.send({result:"Postman Data Inserted Success"});
+})
+
+// update by postman body   http://localhost:5000/putdata  add data in body
+
+app.put("/putdata", async (req, resp) => {
+    const data = await dbConnect();
+    let result = data.updateOne(
+        { id: req.body.id },// UPDATE FROM column name
+        { $set: req.body }
+    )
+    resp.send({ status: "updated" })
+})
+
+
+// update by postman body & parameter from url  
+// http://localhost:5000/putdatap?id=10
+app.put("/putdatap", async (req, resp) => {
+    const data = await dbConnect();
+	console.log(req.params);
+    let result = data.updateOne(
+        { id: req.query.id },// or id: req.params.id UPDATE FROM column name UPDATE FROM column name
+        { $set: req.body }
+    )
+    resp.send({ status: "updated" })
+})
+
+app.listen(5000);
